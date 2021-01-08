152320ef25761d97c4bb5e5172249826c9e3e45e
Negabinary <negabinary@gmail.com>
2021-01-08 22:11:54 +0000

    Fixed crash on create lambda

--- src/expr_item/locator.gd
+++ src/expr_item/locator.gd
@@ -57,18 +57,18 @@ func abandon_lowest(n:int) -> Locator:
 	return get_script().new(root_expr_item, indeces, expr_item.abandon_lowest(n), self, abandon + n)
 
 
-"""
-func get_proof_box(root_proof_box:ProofBox):
+
+func get_proof_box(root_proof_box):
 	if parent == null:
 		return root_proof_box
 	elif parent.get_type().is_binder() and get_indeces()[-1] == 1:
-		return ProofBox.new(
+		return root_proof_box.get_script().new(
 			[parent.get_child(0).get_type()],
 			parent.get_proof_box(root_proof_box)
 		)
 	else:
 		return parent.get_proof_box(root_proof_box)
-"""
+
 
 func _to_string():
 	return expr_item.to_string()
