531ca61f93e5e81920ba241ce055937f7b36b1b2
Negabinary <negabinary@gmail.com>
2022-05-24 15:29:15 +0100

    Theme changes

--- ui/proof/options/LabelOption.gd
+++ ui/proof/options/LabelOption.gd
@@ -5,5 +5,6 @@ var option:Justification.LabelOption
 func init(option:Justification.LabelOption):
 	self.option = option
 	text = option.get_text()
-	add_color_override("font_color", Color.red if option.get_is_err() else Color.black)
+	if option.get_is_err():
+		add_color_override("font_color", get_color("warning_color","WrittenJustification"))
 	update()
