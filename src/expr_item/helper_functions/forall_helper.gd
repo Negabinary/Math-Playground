fedbe9f3a34bffcf785a1acb12f85d1a062bd90a
Negabinary <negabinary@gmail.com>
2022-10-05 15:55:02 +0100

    Fix conjunction in refinement

--- src/expr_item/helper_functions/forall_helper.gd
+++ src/expr_item/helper_functions/forall_helper.gd
@@ -12,15 +12,27 @@ func _init(new_root:ExprItem):
 	without = _get_without_definitions(new_root)
 
 
-func _get_without_definitions(expr_item:ExprItem):
+func _get_without_definitions(expr_item:ExprItem) -> ExprItem:
 	if expr_item.get_type() == GlobalTypes.FORALL and expr_item.get_child_count() == 2:
 		definitions.append(expr_item.get_child(0).get_type())
 		return _get_without_definitions(expr_item.get_child(1))
 	elif expr_item.get_type() == GlobalTypes.IMPLIES and expr_item.get_child_count() == 2:
-		return ExprItem.new(GlobalTypes.IMPLIES, [
-			expr_item.get_child(0),
-			_get_without_definitions(expr_item.get_child(1))
-		])
+		var conj_terms := []
+		var conj_terms_toexplore := [expr_item.get_child(0)]
+		while not conj_terms_toexplore.empty():
+			var next : ExprItem = conj_terms_toexplore.pop_front()
+			if next.get_type() == GlobalTypes.AND and next.get_child_count() == 2:
+				conj_terms_toexplore.push_front(next.get_child(1))
+				conj_terms_toexplore.push_front(next.get_child(0))
+			else:
+				conj_terms.push_front(next)
+		var result := _get_without_definitions(expr_item.get_child(1))
+		for k in conj_terms:
+			result = ExprItem.new(GlobalTypes.IMPLIES, [
+				k,
+				result
+			])
+		return result
 	else:
 		return expr_item
 
