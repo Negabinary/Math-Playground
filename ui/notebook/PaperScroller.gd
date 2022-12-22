45e62efa59223b8dc581e3af520ad78eab92ca3a
Negabinary <negabinary@gmail.com>
2022-12-22 16:09:08 +0000

    Add page up / page down

--- ui/notebook/PaperScroller.gd
+++ ui/notebook/PaperScroller.gd
@@ -1,5 +1,6 @@
 extends ScrollContainer
 
+
 func _process(_delta):
 	$TextureRect.rect_min_size = Vector2(0, $"%MarginContainer".get_rect().size.y)
 	scroll_vertical = $"%ScrollContainer".scroll_vertical
--- ui/notebook/ScrollBack.gd
+++ ui/notebook/ScrollBack.gd
@@ -10,3 +10,20 @@ func _on_resized():
 	var height_change = get_child(0).rect_size.y - old_size.y
 	old_size = get_child(0).rect_size
 	scroll_vertical = scroll_vertical + height_change
+
+
+func _unhandled_key_input(event):
+	if event.is_action_pressed("ui_page_down"):
+		_page_down()
+		get_tree().set_input_as_handled()
+	elif event.is_action_pressed("ui_page_up"):
+		_page_up()
+		get_tree().set_input_as_handled()
+
+
+func _page_down():
+	scroll_vertical += rect_size.y
+
+
+func _page_up():
+	scroll_vertical -= rect_size.y
